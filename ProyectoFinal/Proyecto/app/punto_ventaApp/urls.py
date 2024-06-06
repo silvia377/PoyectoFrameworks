08a98e193ae92b0da5a5a162b70e8e0e4930eaca
silvia377 <159167011+silvia377@users.noreply.github.com>
2024-06-05 23:05:15 -0600

    Update urls.py
    ya se ven las imagenes

--- ProyectoFinal/Proyecto/app/punto_ventaApp/urls.py
+++ ProyectoFinal/Proyecto/app/punto_ventaApp/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('ventas/', include('ventas.urls')),
     path('usuarios/', include('usuarios.urls')),
 ]
-
-    # urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
